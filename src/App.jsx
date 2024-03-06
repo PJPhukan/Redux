9ede7c0bbd0aadd215a6d97a431aa93ac9656a94
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-03-06 19:15:24 +0530

    React redux

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navber from './Components/Navber'
+import Shop from './Components/Shop'
+
+function App() {
+
+  return (
+    <>
+      <Navber />
+      <Shop />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='container my-3'>
+            <h1>Buy shoes 50 Rs</h1>
+            <button className="btn btn-primary mx-3">-</button>
+            Add to cart this item
+            <button className="btn btn-primary mx-3 ">+</button>
+        </div>
+    )
+}
+
+export default Shop
